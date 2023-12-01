0b9c94217906ca4f55be0430689edd77edfb6326
root <root@dockerHost1.jstart.org>
2023-12-01 12:16:10 -0500

    made folder replacement actually work

--- plugin.php
+++ plugin.php
@@ -9,21 +9,17 @@ Author: Danny Summerlin
 
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-define('FAKE_FOLDER_PATTERN','/\/([\w-]+)$/');
-yourls_add_filter( 'get_shorturl_charset', 'fakeFolder_addUnderscore' );
 function fakeFolder_addUnderscore($in) { return $in.'_'; }
+yourls_add_filter( 'get_shorturl_charset', 'fakeFolder_addUnderscore' );
 
-yourls_add_action('loader_failed','fakeFolder_checkForFakeFolder');
-function fakeFolder_checkForFakeFolder($args) {
-	if(preg_match(FAKE_FOLDER_PATTERN,$args[0])) {
-		yourls_add_filter('redirect_location', 'fakeFolder_useFakeFolder');
-		include( YOURLS_ABSPATH.'/yourls-go.php' );
-		exit;
-	}
+function fakeFolder_useFakeFolder($keyword) {
+	if(strstr($keyword,'/')) {
+		if($keyword[-1] == '/')
+			$keyword = substr($keyword, 0, -1);
+		if($keyword[0] == '/')
+			$keyword = substr($keyword, 1);
+		return str_replace('/','_',$keyword);
+	} else
+		return $keyword;
 }
-function fakeFolder_useFakeFolder($url, $statusCode) {
-	$currentLink = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-	if($currentLink[-1] == '/')
-		$currentLink = substr($currentLink, 0, -1);
-	return preg_replace('/\/([\w-]+)$/',"_$1",$currentLink);
-}
+yourls_add_filter('get_request', 'fakeFolder_useFakeFolder');
